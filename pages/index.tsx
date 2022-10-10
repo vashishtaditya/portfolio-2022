646f49fba559eaae014c68c2a013dffe9ef133cd
Aditya Vashisht <vash0005@algonquinlive.com>
2022-10-10 00:06:21 -0400

    installed dependancies and refactored base setup

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Aditya Vashisht | Portfolio</title>
+      </Head>
+
+      <div></div>
+
+      {/* {header} */}
+
+      {/* {about} */}
+
+      {/* {work experience} */}
+
+      {/* {projects} */}
+
+      {/* {contact} */}
+    </div>
+  )
+}
+
+export default Home
